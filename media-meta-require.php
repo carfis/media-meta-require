a8b1c39536ae3839c5c60d1c10db64583f652625
Ralf Koller <r.koller@gmail.com>
2015-03-15 17:05:37 +0100

    Prevent direct access to the plugin

--- media-meta-require.php
+++ media-meta-require.php
@@ -26,3 +26,5 @@ License: GPLv2
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
+
+if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
