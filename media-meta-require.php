b981c70707271fcd29c1fbf6502a670b740556e8
Ralf Koller <r.koller@gmail.com>
2015-03-15 17:20:58 +0100

    Basic UI extension for the media settings
    An extension for the media settings page inside the admin interface

--- media-meta-require.php
+++ media-meta-require.php
@@ -37,3 +37,98 @@ function mmr_install() {
     }
 }
 register_activation_hook( __FILE__, 'mmr_install' );
+
+
+
+//Set up and register a section and fields inside the media options page
+function initialize_media_meta_require_options() {
+     add_settings_section(
+        'media_settings_section',
+        __( 'Which media fields are required for publishing', 'mmrequest'),
+        'initialize_media_meta_require_options_callback',
+        'media'
+    );
+
+    add_settings_field( 
+        'mmr_require_title',
+        __( 'Title', 'mmrequest' ),
+        'mmr_toggle_title_callback',
+        'media',
+        'media_settings_section'
+    );
+    
+     add_settings_field( 
+        'mmr_require_caption',
+        __( 'Caption', 'mmrequest' ),
+        'mmr_toggle_caption_callback',
+        'media',
+        'media_settings_section'
+    );
+
+    add_settings_field( 
+        'mmr_require_alt',                     
+        __( 'Alt Text', 'mmrequest' ),              
+        'mmr_toggle_alt_callback',  
+        'media',                          
+        'media_settings_section'
+    );
+     
+    add_settings_field( 
+        'mmr_require_desc',                      
+        __( 'Description', 'mmrequest' ),               
+        'mmr_toggle_desc_callback',   
+        'media',                          
+        'media_settings_section'
+    );
+
+    register_setting(
+        'media',
+        'mmr_require_title'
+    );
+     
+    register_setting(
+        'media',
+        'mmr_require_caption'
+    );
+     
+    register_setting(
+        'media',
+        'mmr_require_alt'
+    );
+
+    register_setting(
+        'media',
+        'mmr_require_desc'
+    );
+
+    // settings mit get_option('ID') abrufbar
+}
+add_action('admin_init', 'initialize_media_meta_require_options');
+
+//TODO create options array instead of four single options
+
+//About text for the new media section
+function initialize_media_meta_require_options_callback() {
+    echo '<p>' . _e( 'Please choose the descriptive media fields you would like to require to be filled out. Otherwise the user is prohibited to publish the media object.', 'mmrequest' ) . '</p>';
+}
+
+//Creation of the four checkboxes
+function mmr_toggle_title_callback($args) {
+    $html = '<input type="checkbox" id="mmr_require_title" name="mmr_require_title" value="1" ' . checked(1, get_option('mmr_require_title'), false) . '/>';     
+    echo $html;
+}
+
+function mmr_toggle_caption_callback($args) {
+    $html = '<input type="checkbox" id="mmr_require_caption" name="mmr_require_caption" value="1" ' . checked(1, get_option('mmr_require_caption'), false) . '/>'; 
+    echo $html;
+}
+ 
+function mmr_toggle_alt_callback($args) {
+    $html = '<input type="checkbox" id="mmr_require_alt" name="mmr_require_alt" value="1" ' . checked(1, get_option('mmr_require_alt'), false) . '/>'; 
+    echo $html;
+}
+ 
+function mmr_toggle_desc_callback($args) {
+    $html = '<input type="checkbox" id="mmr_require_desc" name="mmr_require_desc" value="1" ' . checked(1, get_option('mmr_require_desc'), false) . '/>'; 
+    echo $html;
+}
