65f568128882abbd74f8b3ec3f6615e0accae041
Ralf Koller <r.koller@gmail.com>
2015-03-26 10:19:47 +0100

    Key rename and cosmetic changes in attachment_meta
    Credential link moved to the readme.md file.
    
    The desc key got adjusted name wise to the options array.

--- media-meta-require.php
+++ media-meta-require.php
@@ -146,13 +146,12 @@ function mmr_attachments_id_array( $arrayofobject ) {
     return $arrayofids;
 }
 
-//https://wordpress.org/ideas/topic/functions-to-get-an-attachments-caption-title-alt-description
 function mmr_attachment_meta( $attachment_id ) {
     $attachment = get_post( $attachment_id );
     return array(
         'alt'           => get_post_meta( $attachment->ID, '_wp_attachment_image_alt', true ),
         'caption'       => $attachment->post_excerpt,
-        'description'   => $attachment->post_content,
+        'desc'          => $attachment->post_content,
         'href'          => get_permalink( $attachment->ID ),
         'src'           => $attachment->guid,
         'title'         => $attachment->post_title
