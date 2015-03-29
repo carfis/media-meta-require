d27a495f881414c78033c6e09fdea1c715f18994
Ralf Koller <r.koller@gmail.com>
2015-03-29 13:40:39 +0200

    Various fixes
    admin notices commented out for the moment
    
    method calls enabled
    
    fixed a few boolean settings

--- media-meta-require.php
+++ media-meta-require.php
@@ -35,7 +35,7 @@ class mmr {
 
         add_action('admin_init', array( $this, 'initialize_media_meta_require_options' ) );
         add_action( 'save_post', array( $this, 'mmr_attachment_check' ), 1, 2);
-        add_action('admin_notices', array( $this, 'myplugin_admin_messages' ) );
+        //add_action('admin_notices', array( $this, 'myplugin_admin_messages' ) );
     }
 
     public function preprint($s, $return=false) {
@@ -170,28 +170,28 @@ class mmr {
     }
 
     public function mmr_attachment_check( $post_id, $post ) {
-
         $mmr_options_array = get_option( 'mmr_options' );
-        $media = mmr_attachments( $post_id );
-        $attachmentids = mmr_attachments_id_array( $media );
+        $media = $this->mmr_attachments( $post_id );
+        $attachmentids = $this->mmr_attachments_id_array( $media );
         $attachment_check_results = array();
         if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
         if (!current_user_can('edit_post', $post_id)) return;
         if( isset($post->post_status) && 'publish' == $post->post_status ) {
             foreach( $attachmentids as $index ) {
-                $attachementmeta = mmr_attachment_meta( $index );
-                $attachment_check_results[ $index ]['title'] = ( !isset( $mmr_options_array['title'] ) || ( isset( $mmr_options_array['title'] ) && !empty( $attachementmeta['title'] ) ) ) ? $attachment_check_results[ $index ]['title'] = "True" : $attachment_check_results[ $index ]['title'] = "False";
-                $attachment_check_results[ $index ]['caption'] = ( !isset( $mmr_options_array['caption'] ) || ( isset( $mmr_options_array['caption'] ) && !empty( $attachementmeta['caption'] ) ) ) ? $attachment_check_results[ $index ]['caption'] = "True" : $attachment_check_results[ $index ]['caption'] = "False" ;
-                $attachment_check_results[ $index ]['alt'] = ( !isset( $mmr_options_array['alt'] ) || ( isset( $mmr_options_array['alt'] ) && !empty( $attachementmeta['alt'] ) ) ) ? $attachment_check_results[ $index ]['alt'] = "True" : $attachment_check_results[ $index ]['alt'] = "False";
-                $attachment_check_results[ $index ]['desc'] = ( !isset( $mmr_options_array['desc'] ) || ( isset( $mmr_options_array['desc'] ) && !empty( $attachementmeta['desc'] ) ) ) ? $attachment_check_results[ $index ]['desc'] = "True" : $attachment_check_results[ $index ]['desc'] = "False";
+                $attachementmeta = $this->mmr_attachment_meta( $index );
+                $attachment_check_results[ $index ]['title'] = ( !isset( $mmr_options_array['title'] ) || ( isset( $mmr_options_array['title'] ) && !empty( $attachementmeta['title'] ) ) ) ? $attachment_check_results[ $index ]['title'] = True : $attachment_check_results[ $index ]['title'] = False;
+                $attachment_check_results[ $index ]['caption'] = ( !isset( $mmr_options_array['caption'] ) || ( isset( $mmr_options_array['caption'] ) && !empty( $attachementmeta['caption'] ) ) ) ? $attachment_check_results[ $index ]['caption'] = True : $attachment_check_results[ $index ]['caption'] = False;
+                $attachment_check_results[ $index ]['alt'] = ( !isset( $mmr_options_array['alt'] ) || ( isset( $mmr_options_array['alt'] ) && !empty( $attachementmeta['alt'] ) ) ) ? $attachment_check_results[ $index ]['alt'] = True : $attachment_check_results[ $index ]['alt'] = False;
+                $attachment_check_results[ $index ]['desc'] = ( !isset( $mmr_options_array['desc'] ) || ( isset( $mmr_options_array['desc'] ) && !empty( $attachementmeta['desc'] ) ) ) ? $attachment_check_results[ $index ]['desc'] = True : $attachment_check_results[ $index ]['desc'] = False;
                 if( in_array( False, $attachment_check_results[ $index ] ) ) {
-                    $attachment_check_fail_array = array_keys( $attachment_check_results[ $index ], "False" );
+                    $attachment_check_fail_array = array_keys( $attachment_check_results[ $index ], False );
                     $attachment_check_fail_output = implode( ', ', $attachment_check_fail_array);
                     $erroroutput = 'Prevent Publish: The fields ' . $attachment_check_fail_output . ' in the image with the ID of ' . $index . ' need some care and love. Afterwards you will be able to publish them.';
-                    //echo $erroroutput;
-                    $this->$prevent_publish = True;
+                    $this->preprint($erroroutput);
+                    $this->prevent_publish = True;
                 }
             }
+            $this->preprint($attachment_check_results);
             if ( $this->prevent_publish) {
                 remove_action( 'save_post', 'my_save_post' );
                 wp_update_post( array( 'ID' => $post_id, 'post_status' => 'draft' ) );
@@ -200,15 +200,15 @@ class mmr {
         }
     }
 
-    public function myplugin_admin_messages() {
-        if ( $this->prevent_publish ) {
-            add_settings_error( 'mmr-notices', 'mmr-attachment-check-fail', __(  'Failed', 'mmrequest'), 'error' );
-        }
-        else {
-            add_settings_error( 'mmr-notices', 'mmr-attachment-check-success', __('Worky Work', 'mmrequest'), 'updated' );
-        }
-        settings_errors( 'mmr-notices' );
-    }
+    // public function myplugin_admin_messages() {
+    //     if ( $this->prevent_publish ) {
+    //         add_settings_error( 'mmr-notices', 'mmr-attachment-check-fail', __(  'Failed', 'mmrequest'), 'error' );
+    //     }
+    //     else {
+    //         add_settings_error( 'mmr-notices', 'mmr-attachment-check-success', __('Worky Work', 'mmrequest'), 'updated' );
+    //     }
+    //     settings_errors( 'mmr-notices' );
+    // }
 }
 
 function media_media_require_launch() {
